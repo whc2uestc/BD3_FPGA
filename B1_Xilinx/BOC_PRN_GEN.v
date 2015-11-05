140cd8f5b75fe2fc5f98f67d6a27e7f4bc1a6c90
HC.Wang <171094685@qq.com>
2015-11-05 11:34:25 +0800

    update.

--- B1_Xilinx/BOC_PRN_GEN.v
+++ B1_Xilinx/BOC_PRN_GEN.v
@@ -1,10 +1,25 @@
-module BOC_PRN_GEN(rx_clk,rx_rst,
-					tx_loc_boc,tx_loc_prn);
+module BOC_PRN_GEN(rx_clk,rx_rst,rx_prn_fcw,rx_corr_paral,
+					tx_loc_boc,tx_loc_prn,tx_prn_sop,tx_prn_eop);
 
 parameter ACC_WIDTH = 32;
 input rx_clk,rx_rst;
-
+input[ACC_WIDTH-1:0] rx_prn_fcw;
+input[1:0] rx_corr_paral;
 output tx_loc_boc,tx_loc_prn;
+output tx_prn_sop,tx_prn_eop;
 
+reg[ACC_WIDTH-1:0] phs_acc_reg;
+
+always @(posedge rx_clk) begin
+	if(rx_rst) begin
+		if(rx_corr_paral[1]) 
+			phs_acc_reg <= rx_prn_fcw;
+		else
+			phs_acc_reg <= {ACC_WIDTH{1'b0}};
+	end 
+	else begin
+		
+	end
+end
 
 endmodule
