515aa60386c9433084a867bb69371f09dedd8d56
汪寒成 <171094685@qq.com>
2015-11-09 13:54:55 +0800

    the car generator of carrier.

--- B1_Xilinx/CAR_GEN.v
+++ B1_Xilinx/CAR_GEN.v
@@ -1,3 +1,23 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    14:11:47 11/07/2015 
+// Design Name: 	whc
+// Module Name:    CAR_GEN
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
 module CAR_GEN(rx_clk,rx_rst,rx_car_fcw,
 				tx_car_cos,tx_car_sin);
 
