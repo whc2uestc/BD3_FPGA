87769dd221598244238d5330cc1ce6ebeb928fe7
汪寒成 <171094685@qq.com>
2015-11-09 13:43:06 +0800

    the acquisition algorithm of pilot channel.

--- B1_Xilinx/TMBOC_ACQ.v
+++ B1_Xilinx/TMBOC_ACQ.v
@@ -10,13 +10,18 @@ parameter CORR_WIDTH = 32;
 parameter PRN_PHS_WIDTH = 14;
 input[7:0] rx_src;
 input rx_clk,rx_rst;
+input[ACC_WIDTH-1:0] rx_prn_fcw,rx_car_fcw;
 output[15:0] tx_src_real;
 output[15:0] tx_src_imag;
-output[ACC_WIDTH-1:0] rx_car_fcw;
 output tx_loc_tmbocE,tx_loc_tmbocP,tx_loc_tmbocL;
 output tx_prn_sop;
 output tx_trk_rst;
 
+
+reg acq_flag;
+reg acq_suc;
+reg[PRN_PHS_WIDTH-1:0] acq_phs;
+wire[PRN_PHS_WIDTH-1:0] prn_phs;
 assign tx_trk_rst = rx_rst | (acq_flag ^ acq_suc);
 
 assign car_fcw = acq_suc?(32'd1342177280 - rx_car_fcw):32'd1342177280;
@@ -24,10 +29,9 @@ assign prn_fcw = acq_suc?(rx_prn_fcw+32'd274609472):32'd274609472;
 
 reg trk_rst;
 reg prn_gen_rst;
-reg acq_suc;
 always @(posedge rx_clk) begin
 	if(rx_rst) begin
-		tx_trk_rst <= 1'b1;
+		prn_gen_rst <= 1'b1;
 		acq_suc <= 1'b0;
 	end
 	else begin
@@ -44,7 +48,7 @@ always @(posedge rx_clk) begin
 	end
 end
 
-wire[PRN_PHS_WIDTH-1:0] prn_phs;
+
 TMBOC_PRN_GEN_TRK TMBOC_PRN_GEN_TRK(.rx_clk(rx_clk),.rx_rst(prn_gen_rst),.rx_prn_fcw(prn_fcw),.rx_corr_paral(3'b0),.rx_init_phs({ACC_WIDTH{1'b0}}),
 						.tx_loc_tmboc(tx_tmloc_bocE),.tx_loc_prn(),.tx_prn_sop(tx_prn_sop),.tx_prn_eop(prn_eop),.tx_prn_phs(prn_phs));
 
@@ -72,17 +76,17 @@ assign tx_src_imag = car_sin;
 CAR_GEN TMBOC_CAR_GEN(.rx_clk(rx_clk),.rx_rst(rx_rst),.rx_car_fcw(car_fcw),
 					.tx_car_cos(car_cos),.tx_car_sin(car_sin));	
 				
-TMBOC_PRN_GEN_ACQ TMBOC_PRN_GEN_U1(.rx_clk(rx_clk),.rx_rst(rx_rst),.rx_prn_fcw(prn_fcw),.rx_corr_paral(3'b100),.rx_init_phs({ACC_WIDTH{1'b0}}),.rx_paral_index(2'b0),
+TMBOC_PRN_GEN_ACQ TMBOC_PRN_GEN_U1(.rx_clk(rx_clk),.rx_rst(rx_rst),.rx_prn_fcw(prn_fcw),.rx_corr_paral(3'b100),.rx_init_phs({ACC_WIDTH{1'b0}}),.rx_paral_index(2'b00),
 						.tx_loc_tmboc(loc_tmboc_1),.tx_loc_prn(),.tx_prn_sop(prn_sop_1),.tx_prn_eop(prn_eop_1));
-TMBOC_PRN_GEN_ACQ TMBOC_PRN_GEN_U2(.rx_clk(rx_clk),.rx_rst(rx_rst),.rx_prn_fcw(prn_fcw),.rx_corr_paral(3'b100),.rx_init_phs({2'b01,{{ACC_WIDTH-2}{1'b0}}}),.rx_paral_index(2'b1),
+TMBOC_PRN_GEN_ACQ TMBOC_PRN_GEN_U2(.rx_clk(rx_clk),.rx_rst(rx_rst),.rx_prn_fcw(prn_fcw),.rx_corr_paral(3'b100),.rx_init_phs({2'b01,{{ACC_WIDTH-2}{1'b0}}}),.rx_paral_index(2'b01),
 						.tx_loc_tmboc(loc_tmboc_2),.tx_loc_prn(),.tx_prn_sop(prn_sop_2),.tx_prn_eop(prn_eop_2));
-TMBOC_PRN_GEN_ACQ TMBOC_PRN_GEN_U3(.rx_clk(rx_clk),.rx_rst(rx_rst),.rx_prn_fcw(prn_fcw),.rx_corr_paral(3'b100),.rx_init_phs({2'b10,{{ACC_WIDTH-2}{1'b0}}}),.rx_paral_index(2'b2),
+TMBOC_PRN_GEN_ACQ TMBOC_PRN_GEN_U3(.rx_clk(rx_clk),.rx_rst(rx_rst),.rx_prn_fcw(prn_fcw),.rx_corr_paral(3'b100),.rx_init_phs({2'b10,{{ACC_WIDTH-2}{1'b0}}}),.rx_paral_index(2'b10),
 						.tx_loc_tmboc(loc_tmboc_3),.tx_loc_prn(),.tx_prn_sop(prn_sop_3),.tx_prn_eop(prn_eop_3));	
-TMBOC_PRN_GEN_ACQ TMBOC_PRN_GEN_U4(.rx_clk(rx_clk),.rx_rst(rx_rst),.rx_prn_fcw(prn_fcw),.rx_corr_paral(3'b100),.rx_init_phs({2'b11,{{ACC_WIDTH-2}{1'b0}}}),.rx_paral_index(2'b3),
+TMBOC_PRN_GEN_ACQ TMBOC_PRN_GEN_U4(.rx_clk(rx_clk),.rx_rst(rx_rst),.rx_prn_fcw(prn_fcw),.rx_corr_paral(3'b100),.rx_init_phs({2'b11,{{ACC_WIDTH-2}{1'b0}}}),.rx_paral_index(2'b11),
 						.tx_loc_tmboc(loc_tmboc_4),.tx_loc_prn(),.tx_prn_sop(prn_sop_4),.tx_prn_eop(prn_eop_4));							
 
-LPM_MULT8_CORE multiI(	.dataa(car_cos),	.datab(rx_src),	.result(data_real));
-LPM_MULT8_CORE multiQ(	.dataa(car_sin),	.datab(rx_src),	.result(data_imag));
+LPM_MULT8_CORE multiI(	.a(car_cos),	.b(rx_src),	.p(data_real));
+LPM_MULT8_CORE multiQ(	.a(car_sin),	.b(rx_src),	.p(data_imag));
 
 CORR_ACC TMBOC_CORR_U1(.rx_clk(rx_clk),.rx_rst(rx_rst),.rx_data_real(data_real),.rx_data_imag(data_imag),
 				.rx_loc_boc(loc_tmboc_1),.rx_prn_sop(prn_sop_1),.rx_prn_eop(prn_eop_1),.tx_corr_acc(corr_acc_1));
@@ -100,12 +104,12 @@ always @(posedge rx_clk) begin
 	if(rx_rst) begin
 		corr_peak <= {CORR_WIDTH{1'b0}};
 		acq_prn_phs <= {PRN_PHS_WIDTH{1'b0}};
-		corr_phs_1 <= {{PRN_PHS_WIDTH-2}{1'b0},2'b00};
-		corr_phs_2 <= {{PRN_PHS_WIDTH-2}{1'b0},2'b01};
-		corr_phs_3 <= {{PRN_PHS_WIDTH-2}{1'b0},2'b10};
-		corr_phs_4 <= {{PRN_PHS_WIDTH-2}{1'b0},2'b11};
+		corr_phs_1 <= {{{PRN_PHS_WIDTH-2}{1'b0}},2'b00};
+		corr_phs_2 <= {{{PRN_PHS_WIDTH-2}{1'b0}},2'b01};
+		corr_phs_3 <= {{{PRN_PHS_WIDTH-2}{1'b0}},2'b10};
+		corr_phs_4 <= {{{PRN_PHS_WIDTH-2}{1'b0}},2'b11};
 	end
-	else if(!tx_acq_suc) begin
+	else if(!acq_suc) begin
 		if(prn_eop_1) begin
 			if(corr_acc_1 > corr_peak) begin
 				corr_peak <= corr_acc_1;
@@ -140,8 +144,8 @@ always @(posedge rx_clk) begin
 	end
 end
 
-reg acq_flag;
-reg[PRN_PHS_WIDTH-1:0] acq_phs;
+
+
 always @(posedge rx_clk) begin
 	if(rx_rst) begin
 		acq_flag <= 1'b0;
