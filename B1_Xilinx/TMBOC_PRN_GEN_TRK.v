abbf97e39ace7ccb4c9a83f0187285be22b007cf
HC.Wang <171094685@qq.com>
2016-05-17 11:03:12 +0800

    The prn code generator of B1 pilot channel tracking.

--- B1_Xilinx/TMBOC_PRN_GEN_TRK.v
+++ B1_Xilinx/TMBOC_PRN_GEN_TRK.v
@@ -1,5 +1,5 @@
 module TMBOC_PRN_GEN_TRK(rx_clk,rx_rst,rx_prn_fcw,rx_corr_paral,rx_init_phs,
-					tx_loc_tmboc,tx_loc_prn,tx_prn_sop,tx_prn_eop,tx_prn_phs);
+					tx_loc_tmboc,tx_loc_prn,tx_prn_sop,tx_prn_eop,tx_prn_phs,tx_phs_acc_reg);
 parameter PRN_PHS_WIDTH = 13;
 parameter ACC_WIDTH = 32;
 input rx_clk,rx_rst;
@@ -8,47 +8,91 @@ input[ACC_WIDTH-1:0] rx_init_phs;
 input[2:0] rx_corr_paral;
 output tx_loc_tmboc,tx_loc_prn;
 output tx_prn_sop,tx_prn_eop;
-output wire[PRN_PHS_WIDTH-1:0] tx_prn_phs;
+output wire[11:0] tx_prn_phs;
+output[31:0] tx_phs_acc_reg;
 
-assign tx_prn_phs = prn_phs[PRN_PHS_WIDTH-2:0];
-assign tx_loc_tmboc = tx_loc_prn ^ phs_acc_reg[31];
+(*keep="yes"*) reg[11:0] prn_phs;
+(*keep="yes"*) reg[11:0] prn_phs_delay;
+(*keep="yes"*) reg[32:0] phs_acc_reg;
+(*keep="yes"*) reg[1:0] sub_boc;
+(*keep="yes"*) reg sub_tmboc;
+//(*keep="yes"*) reg tx_loc_prn;
+assign tx_prn_phs = prn_phs;
+assign tx_loc_tmboc = sub_boc[1]^tx_loc_prn;
+assign tx_phs_acc_reg = phs_acc_reg[31:0];
 
-reg[ACC_WIDTH-1:0] phs_acc_reg;
-reg phs_acc_reg_delay;
-reg[PRN_PHS_WIDTH-1:0] prn_phs;
-reg tx_prn_sop,tx_prn_eop;
+
+(*keep="yes"*) reg phs_acc_reg_delay;
+(*keep="yes"*) reg tx_prn_sop,tx_prn_eop;
+(*keep="yes"*) reg[7:0] sub_phs;
 always @(posedge rx_clk) begin
 	if(rx_rst) begin
-		if(rx_corr_paral[2]) 
-			phs_acc_reg <= rx_init_phs;
-		else
-			phs_acc_reg <= {ACC_WIDTH{1'b0}};
+		phs_acc_reg <= 33'b0;
 		phs_acc_reg_delay <= 1'b0;
-		prn_phs <= {PRN_PHS_WIDTH{1'b0}};
+		prn_phs <= 12'b0;
 		tx_prn_sop <= 1'b0;
 		tx_prn_eop <= 1'b0;
+		sub_boc <= 2'b0;
+		prn_phs_delay <= 12'd4091;
+		sub_phs <= 8'd0;
 	end 
 	else begin
-		if(!tx_prn_sop && prn_phs=={PRN_PHS_WIDTH{1'b0}})
+		if(prn_phs==12'd0 && prn_phs_delay==12'd4091)
 			tx_prn_sop <= 1'b1;
 		else
 			tx_prn_sop <= 1'b0;
-		if(!tx_prn_eop && prn_phs[PRN_PHS_WIDTH-1])
+		if(prn_phs==12'd4091 && prn_phs_delay==12'd4090)
 			tx_prn_eop <= 1'b1;
 		else
 			tx_prn_eop <= 1'b0;
-			
+		prn_phs_delay <= prn_phs;
 		phs_acc_reg <= phs_acc_reg + rx_prn_fcw;
 		phs_acc_reg_delay <= phs_acc_reg[ACC_WIDTH-2];
-		if(prn_phs == 13'd4091)
-			prn_phs <= {PRN_PHS_WIDTH{1'b0}};
-		else if(phs_acc_reg[ACC_WIDTH-2]^phs_acc_reg_delay)
-			prn_phs <= prn_phs + 1;
-		
+		if(phs_acc_reg[32]) begin
+			if(prn_phs == 12'd4091)
+				prn_phs <= 12'b0;
+			else
+				prn_phs <= prn_phs + 1;
+			sub_boc <= sub_boc + 2'd1;
+			phs_acc_reg[32] <= 1'b0;
+			
+			
+//			if(sub_phs==8'd131)
+//				sub_phs <= 8'd0;
+//			else
+//				sub_phs <= sub_phs + 8'd1;
+		end
 	end
 end
 
+//
+//reg[32:0] sub6_phs_acc_reg;
+//wire[31:0] sub6_fcw;
+//assign sub6_fcw = rx_prn_fcw + {rx_prn_fcw[30:0],1'b0};
+//always @(posedge rx_clk) begin
+//	if(rx_rst)
+//		sub6_phs_acc_reg <= 33'b0;
+//	else begin
+//		sub6_phs_acc_reg <= sub6_phs_acc_reg + {sub6_fcw[31],sub6_fcw};
+//	end
+//end
+//
+//always @(posedge rx_clk) begin
+//	if(rx_rst)
+//		sub_tmboc <= 1'b0;
+//	else begin
+//		if(sub_phs[7:2]==6'd0 || sub_phs[7:2]==6'd4 || sub_phs[7:2]==6'd6 || sub_phs[7:2]==6'd29 )
+//			sub_tmboc <= sub6_phs_acc_reg[32];
+//		else
+//			sub_tmboc <= sub_boc[1];
+//	end
+//end
+//wire loc_prn;
 // µ÷ÓÃROM IP Core 
-TMBOC_PRN_TRK_ROM TMBOC_PRN_TRK(	.address(phs_acc_reg[31:20]),	.clock(rx_clk),	.q(tx_loc_prn));
+TMBOC_PRN_TRK_ROM TMBOC_PRN_TRK(	.A(prn_phs),	.SPO(tx_loc_prn));
+
+//always @(posedge rx_clk) begin
+//	tx_loc_prn <= loc_prn;
+//end
 
 endmodule
