a3a0e46bef546e94e202e140caad8f8e86983770
HC.Wang <171094685@qq.com>
2016-05-17 10:53:47 +0800

    the carrier generator.

--- B1_Xilinx/CAR_GEN.v
+++ B1_Xilinx/CAR_GEN.v
@@ -1,23 +1,21 @@
 module CAR_GEN(rx_clk,rx_rst,rx_car_fcw,
-				tx_car_cos,tx_car_sin);
+				tx_car_cos,tx_car_sin,phs_acc_reg);
 
-parameter ACC_WIDTH = 32;
-parameter CAR_WIDTH = 8;
 input rx_clk,rx_rst;
-input [ACC_WIDTH-1:0] rx_car_fcw;
+input [31:0] rx_car_fcw;
 
-output wire[CAR_WIDTH-1:0] tx_car_cos,tx_car_sin;
-
-reg[ACC_WIDTH-1:0] phs_acc_reg;
+output wire[7:0] tx_car_cos,tx_car_sin;
+output[31:0] phs_acc_reg;
+(*keep="yes"*) reg[31:0] phs_acc_reg;
 always @(posedge rx_clk) begin
-	if(rx_rst == 1)
-		phs_acc_reg <= {ACC_WIDTH{1'b1}};
+	if(rx_rst)
+		phs_acc_reg <= {32{1'b1}};
 	else
 		phs_acc_reg <= phs_acc_reg + rx_car_fcw;
 end
 
 // µ÷ÓÃROM IP Core 
-CAR_COS_ROM CAR_I(	.address(phs_acc_reg[31:24]),	.clock(clk),	.q(tx_car_cos));
-CAR_COS_ROM CAR_Q(	.address(phs_acc_reg[31:24]+8'd192),	.clock(clk),	.q(tx_car_sin));
+CAR_COS_ROM CAR_I(	.A(phs_acc_reg[31:24]),	.SPO(tx_car_cos));
+CAR_COS_ROM CAR_Q(	.A(phs_acc_reg[31:24]+8'd192),	.SPO(tx_car_sin));
 
 endmodule
